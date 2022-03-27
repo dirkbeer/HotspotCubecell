7e1fd4409c30a9b832a43d55093ee21c7ba3fc2f
Dirk Beer <rdbeer@pm.me>
2022-03-26 19:44:32 -0700

    Revert "separate sensor reading code"
    This reverts commit 7956e10cccadb6cdf4d12a1a918a73353d58779c.

--- src/main.cpp
+++ src/main.cpp
@@ -77,5 +77,27 @@ void setup() {
 }
 
 void loop() {
+  switch( u8state ) {
+  case 0: 
+    if (millis() > u32wait) u8state++; // wait state
+    break;
+  case 1: 
+    telegram.u8id = 0x01; // slave address
+    telegram.u8fct = 0x03; // function code (this one is registers read)
+    telegram.u16RegAdd = 0x0101; // start address in slave
+    telegram.u16CoilsNo = 1; // number of elements (coils or registers) to read
+    telegram.au16reg = au16data; // pointer to a memory array in the Arduino
 
+    master.query( telegram ); // send query (only once)
+    u8state++;
+    break;
+  case 2:
+    master.poll(); // check incoming messages
+    if (master.getState() == COM_IDLE) {
+      u8state = 0;
+      u32wait = millis() + 2000; 
+        Serial.println(au16data[0]);//Or do something else!
+    }
+    break;
+  }
 }
