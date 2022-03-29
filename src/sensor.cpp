94fa0fe0a5f3bc877cc62769c782c99b30e5fe51
Dirk Beer <rdbeer@pm.me>
2022-03-29 14:33:34 -0700

    removed turn sensor power off to fix bad readings

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -10,12 +10,12 @@ void sensor_setup(){
 	pinMode(Vext,OUTPUT);
 	digitalWrite(Vext,LOW);
 	hdc1080.begin(0x40);
-	digitalWrite(Vext,HIGH);
+	//digitalWrite(Vext,HIGH); // turning sensor power off causes erroneous readings, but maybe delay would fix? how to correctly implement delay in loop?
 }
 
 void sensor_read(){
-	digitalWrite(Vext,LOW);
+	//digitalWrite(Vext,LOW);
 	temperature = (float)(hdc1080.readTemperature());
     humidity = (float)(hdc1080.readHumidity());
-	digitalWrite(Vext,HIGH);
+	//digitalWrite(Vext,HIGH); // turning sensor power off causes erroneous readings, but maybe delay would fix? how to correctly implement delay in loop?
 }
